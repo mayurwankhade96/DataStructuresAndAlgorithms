9a94cafbe58c8964ebfd4b10c3b0eaa45097f8dd
Mayur Wankhade <mayur.wankhade2@gmail.com>
2021-06-17 19:52:23 +0530

    [Mayur] Add : Welcome Message added.

---
+++ DataStructuresAndAlgorithms/DataStructuresAndAlgorithms/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataStructuresAndAlgorithms
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Data Structures and Algorithms Practice Problems!");
+        }
+    }
+}
