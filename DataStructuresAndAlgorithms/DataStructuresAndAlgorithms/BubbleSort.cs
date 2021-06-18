12e0abce8ca35017ef8213d5bbcd66f084809e00
Mayur Wankhade <mayur.wankhade2@gmail.com>
2021-06-18 17:15:03 +0530

    [Mayur] Add : Bubble sort class added.

---
+++ DataStructuresAndAlgorithms/DataStructuresAndAlgorithms/BubbleSort.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructuresAndAlgorithms
+{
+    class BubbleSort
+    {
+        public int temp;
+        public int[] array = { 12, 25, 9, 34, 21, 17, 29 };
+
+        /// <summary>
+        /// Method to sort array
+        /// </summary>
+        public void Sort()
+        {
+            for (int i = 0; i < array.Length - 1; i++) // For number of pass 
+            {
+                for (int j = 0; j < array.Length - 1 - i; j++) // For comparison in each pass
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Method to print array
+        /// </summary>
+        public void ToPrint()
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
--- DataStructuresAndAlgorithms/DataStructuresAndAlgorithms/Program.cs
+++ DataStructuresAndAlgorithms/DataStructuresAndAlgorithms/Program.cs
@@ -6,7 +6,28 @@ namespace DataStructuresAndAlgorithms
     {
         static void Main()
         {
-            Console.WriteLine("Welcome to Data Structures and Algorithms Practice Problems!");
+            int programNumber = 0;
+            do
+            {
+                Console.WriteLine("***Welcome to Data Structures and Algorithms Practice Problems!***" + "\n");
+                Console.WriteLine("Press 4. For Bubble Sort");
+                programNumber = Convert.ToInt32(Console.ReadLine());
+
+                switch (programNumber)
+                {
+                    case 4:
+                        BubbleSort B = new BubbleSort();
+                        B.ToPrint();
+                        Console.WriteLine("After sorting : ");
+                        B.Sort();
+                        B.ToPrint();
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid Input! Please enter a valid choice...");
+                        break;
+                }
+            } while (programNumber <= 0 || programNumber > 4);
         }
     }
 }
